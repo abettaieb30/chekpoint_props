10ee570278ddc62d66063a4756b0a7aa7d60e1e0
abettaieb30 <amor.bettaieb@gmail.com>
2022-07-30 11:34:13 +0100

    work's doone2

--- check_props/src/App.js
+++ check_props/src/App.js
@@ -1,9 +1,9 @@
-
 import './App.css';
 import Profile from './Profile/Profile';
+import img from "./Profile/me.jpg"
 function App() {
   const handleName=()=>{
-    alert("Amor Bettayeb" ); 
+    alert(Profile.name); 
   }
 
   return ( 
@@ -14,10 +14,11 @@ function App() {
   <div className='wave -two'></div>
   <div className='wave -three'></div>
 </div>
- 
  <Profile  name="Amor Bettayeb"  bio="Je suis un étudiant en classe terminal des études d'ingénierie, la spécialité est les systèmes
 embarques. je suis a la recherche d'un stage PFE dans une société où je peux acquérir plus de
-connaissances et d'expériences et exploiter mes qualifications" profession="Ingénieur éléctrique"  handleName ={handleName}/> 
+connaissances et d'expériences et exploiter mes qualifications" profession="Ingénieur éléctrique"  handleName ={handleName}>
+  <img src={img} alt="" id='phme'/>
+  </Profile> 
 
 </div>
       
--- check_props/src/Profile/Profile.js
+++ check_props/src/Profile/Profile.js
@@ -1,20 +1,27 @@
-import React from 'react'
-import img from "./me.jpg"
-const Profile = (props) => {
+import React from 'react';
+import PropTypes from 'prop-types';
+const Profile = ({name,profession,bio,handleName ,children}) => {
   return (
      
     <div className='con'>
-       <img src={img} alt="" id='phme'/>
-      <h1 className='con1'> my name is  {props.name}   </h1>
-    <h2>Profession: {props.profession}</h2>
-    <p> Bio: {props.bio}</p>
-    <button className='btn' onClick={props.handleName}>Want the profile Handler name?</button> 
+       {children}
+      <h1 className='con1'> my name is  {name}   </h1>
+    <h2>Profession: {profession}</h2>
+    <p> Bio: {bio}</p>
+    <button className='btn' onClick={()=>handleName()}>Want the profile Handler name?</button> 
     
     </div>
     
 
-  )
+  );
   
 }
+Profile.propTypes = { 
+  name: PropTypes.bool,
+  bio: PropTypes.string,
+  profession: PropTypes.string, handleName: PropTypes.func}
 
-export default Profile
+  Profile.defaultProps = {name : "Ned Stark",bio: "NO Bio!!",profession: "jobless?" 
+  };
+ 
+export default Profile;
